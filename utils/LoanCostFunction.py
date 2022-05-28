82cbfb6de98eb0d5a55eadd6d0879115ffd1a622
Daksh Gupta <78641951+dakshgupta2002@users.noreply.github.com>
2022-05-28 21:08:06 +0530

    Update LoanCostFunction.py

--- utils/LoanCostFunction.py
+++ utils/LoanCostFunction.py
@@ -1,44 +1,47 @@
-revenue = 0  
-profit_percentage_last_year = 0 
-inflation=0
 
-simple_interest = 0 
-interest_rate = 0 
-amount_of_loan = 0 
-time_to_repay = 0 
-past_debt_to_repay = 0 
+def loan_cost_prediction(revenue,
+                         profit_percentage_last_year,
+                         simple_interest,
+                         interest_rate,
+                         amount_of_loan,
+                         time_to_repay,
+                         past_debt_to_repay,
+                         sustainability,
+                         competition_scale,
+                         management_effectivity,
+                         startup,
+                         cibil_score, 
+                         inflation = 4 ):
 
-sustainability = 0 
-competition_scale = 0 
-management_effectivity = 0 
-startup = 0 
-cibil_score = 0 
+    # balance the decrease in amount of money
+    interest_rate -= inflation
 
-#balance the decrease in amount of money
-interest_rate -= inflation
+    # substract from the net revenue
+    external_cost = (len(str(amount_of_loan))-1)*(
+        5*(1-sustainability) +
+        3*(1-management_effectivity) +
+        2*(startup) +
+        10*(900 - cibil_score)/100 +
+        3*(competition_scale)
+    )
 
-# substract from the net revenue
-external_cost = (len(str(amount_of_loan))-1)*(
-    5*(1-sustainability) + 
-    3*(1-management_effectivity) +
-    2*(startup) + 
-    10*(900 - cibil_score)/100
-)   
+    loan_interest_cost = amount_of_loan * \
+        (1+interest_rate*time_to_repay) if simple_interest else amount_of_loan * \
+        (pow((1 + interest_rate/100), time_to_repay))
 
-loan_interest_cost = amount_of_loan*(1+interest_rate*time_to_repay) if simple_interest else amount_of_loan*(pow((1 + interest_rate/100),time_to_repay))
+    # the net profit conceded over the period of time
+    net_revenue = revenue*profit_percentage_last_year*time_to_repay
 
-#the net profit conceded over the period of time
-net_revenue = revenue*profit_percentage_last_year*time_to_repay
+    residue_left_after_time = net_revenue - external_cost - loan_interest_cost - past_debt_to_repay
 
-residue_left_after_time = net_revenue - external_cost - loan_interest_cost
-
-if residue_left_after_time < 0:
-    if abs(residue_left_after_time) > 0.1*net_revenue:
-        print("Very High Risk involved")
+    if residue_left_after_time <= 0:
+        if abs(residue_left_after_time) > 0.1*net_revenue:
+            return -2 # High Loss
+        else:
+            return -1 # small loss
     else:
-        print("Moderate risk involved")
-else:
-    if (residue_left_after_time) < 0.1*net_revenue:
-        print("It is fine to grant the loan")
-    else:
-        print("There is a huge scope of expansion in the business")
+        if (residue_left_after_time) < 0.1*net_revenue:
+            return 1 # small profit
+        else:
+            return 2 # high profit
+
